880d69a3ecd2e89b8dbf503d108e24f77e9fcbd9
Artem Krisanov <a.krisanov1@mail.ru>
2025-03-22 22:12:38 +0300

    Initial Commit

---
+++ UdderTests/UdderTests.swift
@@ -0,0 +1,17 @@
+//
+//  UdderTests.swift
+//  UdderTests
+//
+//  Created by Артем Крисанов on 22.03.2025.
+//
+
+import Testing
+@testable import Udder
+
+struct UdderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
